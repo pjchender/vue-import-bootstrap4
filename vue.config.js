020134891e185f5ebd5748aec346198ecab99f13
pjchender <pjchender@gmail.com>
2019-11-07 16:39:30 +0800

    Update to @vue/cli@4

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'windows.jQuery': 'jquery',
+      }),
+    ],
+  },
+};
